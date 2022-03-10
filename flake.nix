0d6792fad986b206e31ac0511dc1ee1f0ff02880
Thibault Gagnaux <tgagnaux@gmail.com>
2022-03-10 09:30:58 +0100

    chore: updates frege-gradle-plugin -> 2.0.1
    - adds a nix flake to have a javafx 11 always ready

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    description            = "frege-data-science-dev-shell";
+    inputs.flake-utils.url = "github:numtide/flake-utils";
+    inputs.nixpkgs.url     = "github:NixOS/nixpkgs";
+
+    outputs = 
+    {
+        self,
+        nixpkgs,
+        flake-utils 
+    }:
+    flake-utils.lib.eachSystem
+    [
+        "x86_64-darwin" 
+        "aarch64-darwin" 
+        "x86_64-linux" 
+    ]
+    (system:
+        let pkgs = import nixpkgs { inherit system; };
+    in
+    {
+        devShell = pkgs.mkShell
+        {
+            buildInputs =
+            [
+                (pkgs.openjdk11.override { enableJavaFX = true; })
+            ];
+        };
+    });
+}
